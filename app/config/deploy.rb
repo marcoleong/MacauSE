f03ce2ed0cf69170a68669f40d4264feb4f9527c
Leong Marco <marcogood411@gmail.com>
2012-02-24 00:09:58 +0800

    fixing deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -4,7 +4,7 @@ set :deploy_to,   "/Users/marcoleong/www/www.mgcse.org.mo"
 set :app_path,    "app"
 
 set :repository,  "skyveostudio.com:/git/www.mgcse.org.mo.git"
-set :scm,         :git
+set :scm,         :rsync_with_remote_cache
 set :use_sudo,     false
 
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, `subversion` or `none`
@@ -15,10 +15,14 @@ role :db,         domain, :primary => true       # This is where Rails migration
 
 set  :keep_releases,  3
 
+set :app_path, "app"
+set :web_path, "web"
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+
 set :shared_files,      ["app/config/parameters.yml"]
 set :shared_children,     ["vendor"]
 
+set :user, "marcoleong"
 
-
-set :update_vendors, false
+set :update_vendors, true
 
