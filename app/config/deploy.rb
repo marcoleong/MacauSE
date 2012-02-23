5b100cdc612e1fceb7572255229a3dc4875d7933
Leong Marco <marcogood411@gmail.com>
2012-02-24 00:06:19 +0800

    modified deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,10 +1,12 @@
 set :application, "Macau Social Entreprise"
-set :domain,      "www.mgcse.org.mo"
+set :domain,      "skyveostudio.com"
 set :deploy_to,   "/Users/marcoleong/www/www.mgcse.org.mo"
 set :app_path,    "app"
 
 set :repository,  "skyveostudio.com:/git/www.mgcse.org.mo.git"
 set :scm,         :git
+set :use_sudo,     false
+
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web,        domain                         # Your HTTP server, Apache/etc
@@ -12,3 +14,10 @@ role :app,        domain                         # This may be the same as your 
 role :db,         domain, :primary => true       # This is where Rails migrations will run
 
 set  :keep_releases,  3
+
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     ["vendor"]
+
+
+set :update_vendors, true
+
